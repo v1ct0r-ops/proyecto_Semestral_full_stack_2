908e05d09184f1f0010d5580364fb4090f186e1d
victor <v.garces@duocuc.cl>
2025-09-08 13:51:51 -0300

    se agrega validacion de contacto js

--- js/contacto.js
+++ js/contacto.js
@@ -1,6 +1,7 @@
 //necesito saber si el input con id nombre correo y contenido tienen algo escrito
 //si tienen algo escrito que el boton de enviar se habilite
 //si no tienen nada escrito que el boton de enviar se deshabilite
+const formulario = document.querySelector('.form-contacto');
 const nombre = document.getElementById("nombre");
 const correo = document.getElementById("correo");
 const contenido = document.getElementById("contenido");
@@ -13,16 +14,63 @@ function validarFormulario() {
     botonEnviar.disabled = true;
   }
 }
+
+function validarUsuario(correo){
+  let eRegular = /^[a-zA-Z0-9._%+-]+@duoc\.cl$/;
+
+  if(correo === ''){
+    alert('El correo no puede estar vacio');
+    return false;
+  } else if(!eRegular.test(correo)){
+    alert('El correo debe ser del tipo usuario@duoc.cl');
+    return false;
+  }
+  else if(correo.length > 20 ){
+    alert('Importante: el correo no puede superar los 20 caracteres.');
+    return false;
+  } else if (/\s/.test(correo)) {
+  alert('El correo no puede contener espacios.');
+  return false;
+  }
+  return true;
+}
+
+function validarNombre(nombre){
+   let eRegularNombre = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+
+   if(!eRegularNombre.test(nombre)){
+    alert('El nombre debne contener solo caracteres alfabeticos y espacios.');
+      return false;
+   }
+   return true;
+}
+
+function validarContenidoSeguro(contenido) {
+  let regex = /[<>]/;
+  if (regex.test(contenido)) {
+    alert('El mensaje no puede contener < o >');
+    return false;
+  }
+  return true;
+}
  
 nombre.addEventListener("input", validarFormulario);
 correo.addEventListener("input", validarFormulario);
 contenido.addEventListener("input", validarFormulario);
 
-botonEnviar.addEventListener("click", (e) => {
-    console.log("Nombre:", nombre.value);
-    console.log("Correo:", correo.value);
-    console.log("Contenido:", contenido.value);
-    alert("Mensaje enviado");
+formulario.addEventListener("submit", function(e) {
+  // Valida correo antes de enviar
+  if (!validarUsuario(correo.value) || !validarNombre(nombre.value) || !validarContenidoSeguro(contenido.value)) {
+    e.preventDefault(); // Detiene el envío si no es válido
+    return;
+  }
+  // Si pasa la validación, muestra el query string
+  const formData = new FormData(formulario);
+  const urlParam = new URLSearchParams(formData);
+  const queryString = urlParam.toString();
+  alert(queryString);
+  
+  e.preventDefault(); 
 });
 
 
